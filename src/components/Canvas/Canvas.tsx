0963c5927ea143b24823e46578bcfbec59c108ec
rizwan <rizwanahamadcodes@gmail.com>
2024-01-18 22:16:02 +0545

    started working on the canvas zoom

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -8,6 +8,7 @@ const Canvas = (props: CanvasProps) => {
     const {} = props;
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+    const [zoomLevel, setZoomLevel] = useState(1);
     const currentProject = useCurrentProject();
 
     useEffect(() => {
@@ -18,12 +19,60 @@ const Canvas = (props: CanvasProps) => {
             });
             setCanvas(newCanvas);
         }
-    }, [currentProject.canvasHeight, currentProject.canvasWidth]);
+    }, []);
+
+    useEffect(() => {
+        canvas?.setZoom(zoomLevel);
+        canvas?.setHeight(currentProject.canvasHeight * zoomLevel);
+        canvas?.setWidth(currentProject.canvasWidth * zoomLevel);
+    }, [zoomLevel, canvas]);
+
+    const fabricText: fabric.Textbox = new fabric.Textbox("text", {
+        fontSize: 30,
+        fontFamily: "Arial",
+        editable: false,
+        charSpacing: 1000,
+        left: 0,
+        top: 0,
+        fill: "#000",
+    });
+
+    canvas?.add(fabricText);
 
     return (
-        <canvas ref={canvasRef} className="shadow">
-            Canvas
-        </canvas>
+        <div className="relative">
+            <div>
+                <button
+                    onClick={() => {
+                        setZoomLevel((prevZoom) => prevZoom - 0.1);
+                    }}>
+                    Zoom out
+                </button>{" "}
+                <button
+                    onClick={() => {
+                        setZoomLevel(1);
+                    }}>
+                    Snap to 100
+                </button>
+                <button
+                    onClick={() => {
+                        setZoomLevel((prevZoom) => prevZoom + 0.1);
+                    }}>
+                    Zoom in
+                </button>
+            </div>
+            <div>
+                {/* this div is for the border */}
+                <div
+                    style={{
+                        height: `${currentProject.canvasHeight * zoomLevel}px`,
+                        width: `${currentProject.canvasWidth * zoomLevel}px`,
+                    }}
+                    className="inline-block border border-gray-100">
+                    <canvas ref={canvasRef}>Canvas</canvas>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/OptionsBar/OptionsBar.tsx
+++ src/components/OptionsBar/OptionsBar.tsx
@@ -1,7 +1,15 @@
+import { FullWidthContainer } from "../Container";
+
 type OptionsBarProps = {};
 
 export const OptionsBar = (props: OptionsBarProps) => {
     const {} = props;
 
-    return <div className="border-b border-b-gray-100 h-4">Options bar</div>;
+    return (
+        <div className="border-b border-b-gray-100 h-4">
+            <FullWidthContainer className="flex flex-col justify-center items-start h-full">
+                <p className="font-medium text-gray-500">Options bar</p>
+            </FullWidthContainer>
+        </div>
+    );
 };
