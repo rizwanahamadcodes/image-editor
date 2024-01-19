581ab824f2d8321104f42d38b965d5bc1f13f786
rizwan <rizwanahamadcodes@gmail.com>
2024-01-19 17:28:46 +0545

    switched from linear interpolated values to basic values

--- src/components/EditingWindow/StatusBar/StatusBar.tsx
+++ src/components/EditingWindow/StatusBar/StatusBar.tsx
@@ -7,36 +7,43 @@ type StatusBarProps = {
 
 const StatusBar = (props: StatusBarProps) => {
     const { zoomLevel, setZoomLevel } = props;
-    const [range, setRange] = useState(50);
 
-    const getZoomFromRange = (x: number) => {
-        return 0.062 * x * x - 1.3 * x + 10;
+    const getZoomFromRange = (range: number) => {
+        if (range <= 50) {
+            return ((18 / 10) * range + 10) / 100;
+        } else {
+            return (8 * range - 300) / 100;
+        }
     };
+    const getRangeFromZoom = (zoom: number) => {
+        const scaledZoom = zoom * 100;
 
-    useEffect(() => {
-        console.log(getZoomFromRange(range));
-
-        setZoomLevel(getZoomFromRange(range) / 100);
-    }, [range, setZoomLevel]);
+        if (scaledZoom <= 100) {
+            return (556 / 1000) * scaledZoom - 556 / 1000;
+        } else {
+            return (125 / 1000) * scaledZoom + 375 / 10;
+        }
+    };
 
     return (
         <div>
             <input
                 onChange={(e) => {
-                    setRange(parseInt(e.target.value));
+                    setZoomLevel(getZoomFromRange(parseInt(e.target.value)));
                 }}
                 type="range"
                 className="border-none outline-none accent-gray-700 h-0.25 bg-gray-100"
                 min={0}
                 max={100}
-                value={range}
+                value={getRangeFromZoom(zoomLevel)}
             />
+            {zoomLevel * 100}
             <button
                 onClick={() => {
                     setZoomLevel((prevZoom) => prevZoom - 0.1);
                 }}>
                 Zoom out
-            </button>{" "}
+            </button>
             <button
                 onClick={() => {
                     setZoomLevel(1);
