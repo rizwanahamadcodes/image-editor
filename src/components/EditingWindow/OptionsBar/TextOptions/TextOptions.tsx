1f41896f7f4fc3e988adede7653d43e308a71bad
rizwan <rizwanahamadcodes@gmail.com>
2024-01-22 16:53:29 +0545

    activeObject is now selected before WebFont request is made cause that takes it's sweet time, so even if the user clicks away the font family will still update if the font family was changed while the font was selected

--- src/components/EditingWindow/OptionsBar/TextOptions/TextOptions.tsx
+++ src/components/EditingWindow/OptionsBar/TextOptions/TextOptions.tsx
@@ -19,19 +19,22 @@ const TextOptions = (props: TextOptionsProps) => {
 
     const setFontFamily = (fontFamily: FontFamily) => {
         //  it is important to import the WebFont here inline otherwise app breaks with window not defined error even if it's a client component
+        const activeObject = canvas?.getActiveObject();
+        if (!activeObject || !activeObject.isType("textbox")) {
+            return;
+        }
+
+        const activeTextObject = activeObject as fabric.Textbox;
+
         const WebFont = require("webfontloader");
         WebFont.load({
             google: {
                 families: [fontFamily.value],
             },
+
             active: () => {
-                (canvas?.getActiveObject() as fabric.Textbox).set(
-                    "fontFamily",
-                    fontFamily.value
-                );
-                (
-                    canvas?.getActiveObject() as fabric.Textbox
-                ).canvas?.renderAll();
+                activeTextObject.set("fontFamily", fontFamily.value);
+                canvas?.renderAll();
             },
         });
         setSelectedFontFamily(fontFamily);
