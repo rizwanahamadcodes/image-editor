50aff46888d3ebe00db0038a21afc95c0026ef76
rizwan <rizwanahamadcodes@gmail.com>
2024-01-23 02:40:23 +0545

    added the import to see if vercel fixed itself

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { CurrentUserContext } from "@/context/useCurrentUser";
+import RootLayout from "@/layouts/RootLayout";
 import { store } from "@/store/store";
 import "@/styles/globals.css";
 import type { NextComponentType, NextPage, NextPageContext } from "next";
