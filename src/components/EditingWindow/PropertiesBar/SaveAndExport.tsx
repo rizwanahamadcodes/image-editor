04c54e77cbbde264a09bf3763ebf59aed401fe02
rizwan <rizwanahamadcodes@gmail.com>
2024-04-06 21:46:55 +0545

    added a kebab memu for the save and export buttons

--- src/components/EditingWindow/PropertiesBar/SaveAndExport.tsx
+++ src/components/EditingWindow/PropertiesBar/SaveAndExport.tsx
@@ -1,11 +1,15 @@
 import Button, { ButtonIcon } from "@/components/Button/Button";
+import PopOver from "@/components/PopOver/PopOver";
 import { useActiveProject } from "@/context/useActiveProject";
 import { useCanvas } from "@/context/useCanvas";
+import { useToggle } from "@/hooks/useToggle";
 import { updateProject } from "@/store/slices/projectsSlice";
 import { RootState } from "@/store/store";
 import clsx from "clsx";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { AiOutlineFontSize } from "react-icons/ai";
 import { FaSave } from "react-icons/fa";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 import { PiUploadSimpleBold } from "react-icons/pi";
 import { useDispatch } from "react-redux";
 type SaveAndExportProps = {
@@ -17,6 +21,9 @@ const SaveAndExport = (props: SaveAndExportProps) => {
     const { canvas, setCanvas } = useCanvas();
     const { activeProject, setActiveProject } = useActiveProject();
     const dispatch = useDispatch();
+    const { open, close, toggle, isOpen } = useToggle();
+
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
 
     const saveProject = () => {
         const clonedCanvas = canvas?.clone((clone: any) => {
@@ -44,23 +51,43 @@ const SaveAndExport = (props: SaveAndExportProps) => {
     }, [activeProject]);
 
     return (
-        <div className={clsx("flex gap-1 flex-row", className)}>
-            <Button
-                variant="solid"
-                colorScheme="primary"
-                size="sm"
-                onClick={saveProject}
-                className="!w-full !sm:w-auto">
-                <ButtonIcon icon={FaSave} />
-            </Button>
-            <Button
-                variant="outline"
-                colorScheme="primary"
-                size="sm"
-                className="!w-full !sm:w-auto">
-                <ButtonIcon icon={PiUploadSimpleBold} />
-            </Button>
-        </div>
+        <>
+            <div className="flex md:hidden gap-0.5 relative">
+                <PopOver
+                    isOpen={isOpen}
+                    close={close}
+                    className="right-0"
+                    toggleButtonRefs={[buttonRef]}>
+                    <div className="bg-white gap-0.5 flex flex-col shadow border border-gray-100 rounded-0.625 mt-0.5 right-0 p-0.25">
+                        <Button
+                            variant="solid"
+                            colorScheme="primary"
+                            size="sm"
+                            onClick={saveProject}>
+                            <ButtonIcon icon={FaSave} />
+                            Save
+                        </Button>
+                        <Button
+                            variant="outline"
+                            colorScheme="primary"
+                            size="sm">
+                            <ButtonIcon icon={PiUploadSimpleBold} />
+                            Export
+                        </Button>
+                    </div>
+                </PopOver>
+
+                <Button
+                    onClick={toggle}
+                    buttonRef={buttonRef}
+                    variant="ghost"
+                    size="sm"
+                    regular
+                    colorScheme="gray-200">
+                    <ButtonIcon icon={HiOutlineDotsVertical} />
+                </Button>
+            </div>
+        </>
     );
 };
 
